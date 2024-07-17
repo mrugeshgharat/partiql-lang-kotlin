63cc175bef43949028c060a2ee16fc29e534081b
Alan Cai <caialan@amazon.com>
2024-07-17 16:56:13 -0700

    Change to configure

--- buildSrc/src/main/kotlin/org/partiql/gradle/plugin/publish/PublishPlugin.kt
+++ buildSrc/src/main/kotlin/org/partiql/gradle/plugin/publish/PublishPlugin.kt
@@ -26,6 +26,7 @@ import org.gradle.api.publish.maven.MavenPublication
 import org.gradle.api.publish.maven.plugins.MavenPublishPlugin
 import org.gradle.api.publish.maven.tasks.PublishToMavenRepository
 import org.gradle.jvm.tasks.Jar
+import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.create
 import org.gradle.kotlin.dsl.get
 import org.gradle.kotlin.dsl.getByName
@@ -173,7 +174,8 @@ abstract class PublishPlugin : Plugin<Project> {
                     sign(publishing.publications["maven"])
                 }
             }
-            extensions.getByType(JReleaserExtension::class.java).run {
+
+            configure<JReleaserExtension> {
                 signing.apply {
                     setActive("ALWAYS")
                     armored.set(true)
